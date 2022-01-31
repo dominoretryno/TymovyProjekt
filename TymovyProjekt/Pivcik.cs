60e8b474bd3cab92e7b6b0e8a0c0ff29121b807f
Pivčík <pivcik@netoffice.cz>
2022-01-31 10:47:31 +0100

    třída Pivcik

---
+++ TymovyProjekt/Pivcik.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TymovyProjekt
+{
+    class Pivcik
+    {
+        public int Plat { get; set; }
+        public string Pozice { get; set; }
+
+    }
+}
