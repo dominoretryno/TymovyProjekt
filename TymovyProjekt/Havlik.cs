c56ce60a8bcb94064dd99071faade71cf1d4f809
Havlík <havlik.skola@gmail.com>
2022-01-31 10:47:24 +0100

    Třída Havlik

---
+++ TymovyProjekt/Havlik.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TymovyProjekt
+{
+    class Havlik
+    {
+        public string Jmeno { get; set; }
+        public int Vek { get; set; }
+    }
+}
