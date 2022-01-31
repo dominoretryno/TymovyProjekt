342faef08e0c9606fd89b1799e23477a091014b3
Havlík <havlik.skola@gmail.com>
2022-01-31 10:38:24 +0100

    Založení projektu

---
+++ TymovyProjekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TymovyProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
